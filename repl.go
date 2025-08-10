99df3e931a028cc8543596cbcdd5245d13440b19
olu <oluegbiaye@gmail.com>
2025-08-10 23:42:56 +0200

    Add command handling to REPL with help and exit commands

--- repl.go
+++ repl.go
@@ -7,10 +7,35 @@ import (
 	"strings"
 )
 
-func startRepl() {
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var usableCommands map[string]cliCommand
+
+func init() {
+	usableCommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func statRepl() {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Print("Pokedx > ")
+		fmt.Print("Pokedex > ")
+
 		reader.Scan()
 
 		words := cleanInput(reader.Text())
@@ -18,9 +43,13 @@ func startRepl() {
 			continue
 		}
 
-		commandName := words[0]
-		fmt.Printf("Your command was: %s\n", commandName)
-
+		for _, word := range words {
+			if cmd, ok := usableCommands[word]; ok {
+				cmd.callback()
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
 	}
 }
 
@@ -28,4 +57,19 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func commandExit() error {
+	defer os.Exit(0)
+	return fmt.Errorf("Closing the Pokedex... Goodbye!\n")
+}
+
+func commandHelp() error {
+	helpMsg := "Welcome to the Pokedex!\nUsage:\n\n"
+	for cmd := range usableCommands {
+		helpMsg += cmd + ": " + usableCommands[cmd].description + "\n"
+	}
+
+	fmt.Printf(helpMsg)
+	return nil
 }
