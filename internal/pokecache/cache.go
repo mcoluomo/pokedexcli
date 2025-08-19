4f4fae9e3586e4637531617e04971605bf9d5ac1
olu <oluegbiaye@gmail.com>
2025-08-19 22:39:44 +0200

    Add cache constructor and basic Add/Get methods

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -14,3 +14,25 @@ type cacheEntry struct {
 	createAt time.Time
 	val      []byte
 }
+
+func NewCache(interval time.Time) Cache {
+	reapLoop()
+	return Cache{}
+}
+
+func (c *Cache) Add(url string, data []byte) {
+	c.Entry[url] = cacheEntry{val: data}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	_, ok := c.Entry[key]
+	var data []byte
+	if !ok {
+		return data, ok
+	}
+
+	return c.Entry[key].val, ok
+}
+
+func (c *Cache) reapLoop() {
+}
