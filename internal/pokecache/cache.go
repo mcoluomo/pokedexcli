b4cbe8755d959823971eace50193794a5732aab9
olu <oluegbiaye@gmail.com>
2025-08-20 19:05:05 +0200

    Refactor cache implementation and add tests
    - Use sync.Mutex instead of pointer - Fix field naming and types -
    Implement cache expiration with reapLoop - Add unit tests for Add, Get,
    and expiration

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -7,26 +7,34 @@ import (
 
 type Cache struct {
 	Entry map[string]cacheEntry
-	mutex *sync.Mutex
+	mutex sync.Mutex
 }
 
 type cacheEntry struct {
-	createAt time.Time
-	val      []byte
+	createdAt time.Time
+	val       []byte
 }
 
-func NewCache(interval time.Time) Cache {
-	reapLoop()
-	return Cache{}
+func NewCache(interval time.Duration) *Cache {
+	cache := Cache{Entry: make(map[string]cacheEntry)}
+	go cache.reapLoop(interval)
+	return &cache
 }
 
 func (c *Cache) Add(url string, data []byte) {
-	c.Entry[url] = cacheEntry{val: data}
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.Entry[url] = cacheEntry{createdAt: time.Now(), val: data}
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mutex.Unlock()
+
 	_, ok := c.Entry[key]
 	var data []byte
+
+	defer c.mutex.Unlock()
+
 	if !ok {
 		return data, ok
 	}
@@ -34,5 +42,15 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	return c.Entry[key].val, ok
 }
 
-func (c *Cache) reapLoop() {
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.Tick(interval)
+	for range ticker {
+		c.mutex.Lock()
+		for key := range c.Entry {
+			if time.Since(c.Entry[key].createdAt) > interval {
+				delete(c.Entry, key)
+			}
+		}
+		c.mutex.Unlock()
+	}
 }
